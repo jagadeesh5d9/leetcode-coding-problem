40835e43cea0dcc94a9cca21b7aafe23c0251509
jagadeesh <dasarijagadeesh442@gmail.com>
2025-08-20 21:29:36 +0530

    Dp on matrix..

---
+++ Problem_of_the_day/1277._Count_Square_Submatrices_with_All_Ones.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+        int n = matrix.size();//rows...
+        int m = matrix[0].size();//columns...
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        int ans = 0;
+        //For the 1st row...
+        for(int i=0;i<n;i++){
+            if(matrix[i][0]==1) {
+                dp[i][0] = 1;
+                ans++;
+            }
+        }
+
+        //For the 1st coloum except [0,0] cause already visited...
+        for(int i=1;i<m;i++){
+            if(matrix[0][i]==1){
+                dp[0][i] = 1;
+                ans++;
+            }
+        }
+
+        //Start with [1,1]...
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][j]==1){
+                    dp[i][j] = 1+ min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});//Find min no of squares possible in 3 directions..
+                    ans += dp[i][j];
+                }
+            }
+        }
+        return ans;
+    }
+};
